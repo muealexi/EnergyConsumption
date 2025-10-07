71b7daee151e33341aaa8f316e4354a8168563ed
Alexis <alexis.mueller70@gmail.com>
2025-10-07 20:48:07 +0200

    Unify plots into callable functions, add new data

--- EnergieVerbrauch.py
+++ EnergieVerbrauch.py
@@ -84,49 +84,128 @@ def consumption_table(title, consumption_dict):
     # To
     table.add_row(["To"] + [df.index[end].date() for end in consumption_dict["end"]])
     print(table)
+  
+def plot_rolling_year(df, year_ends, mode='ma'):
+    """
+    Extensible rolling year plot.
+    mode: 'ma', 'cumulative', or any custom string.
+    custom_func: function(df, start_idx, end_idx) -> (x, y_el, y_gas, ylabel, title)
+    """
+
+    plt.figure()
+    colors_el = ['#ffe5e5', '#ffb3b3', '#ff6666', '#cc0000', '#660000', '#2d0000']
+    colors_gas = ['#e0f7fa', '#80deea', '#26c6da', '#00838f', '#004d4d', '#002222']
+    n_years = len(year_ends)
+    start_idx = 0
+
+    
+    for i in range(n_years):
+        end_idx = year_ends[i]
+        color_el = colors_el[i % len(colors_el)]
+        color_gas = colors_gas[i % len(colors_gas)]
+        x = df.Day.iloc[start_idx:end_idx-1] - df.Day.iloc[start_idx]
+        if mode == 'ma':
+            y_el = df.dE_EL_kWh_MA.iloc[start_idx:end_idx-1]
+            y_gas = df.dE_Gas_kWh_MA.iloc[start_idx:end_idx-1]
+            ylabel = 'Energy [kWh/d]'
+            title = 'Moving Average Energy Consumption per Day in kWh (Rolling Year)'
+        elif mode == 'cumulative':
+            y_el = df.Counter_El_kWh.iloc[start_idx:end_idx-1] - df.Counter_El_kWh.iloc[start_idx]
+            y_gas = df.Counter_Gas_kWh.iloc[start_idx:end_idx-1] - df.Counter_Gas_kWh.iloc[start_idx]
+            ylabel = 'Energy [kWh]'
+            title = 'Energy Consumption in kWh (Rolling Year)'
+        else:
+            raise ValueError(f"Unknown mode: {mode}")
+        
+        plt.plot(x, y_el, color=color_el, label=f'Electricity (Y{i+1})')
+        plt.plot(x, y_gas, color=color_gas, label=f'Gas (Y{i+1})')
+        start_idx = end_idx
+    plt.ylabel(ylabel)
+    plt.xlabel('Day')
+    plt.title(title)
+    plt.xticks(rotation=45)
+    plt.grid()
+    plt.legend()
+    plt.tight_layout()
+
+def total_comparison_table(year_labels, consumption_total_el, consumption_total_gas, costs_total_el, costs_total_gas, df, year_ends):
+    print("Total Consumption Comparison over the Years")
+
+    table = PrettyTable(["What"] + year_labels)
+    # Electricity [kWh]
+    table.add_row(["Electricity [kWh]"] + [round(val, 2) for val in consumption_total_el])
+    # Gas [kWh]
+    table.add_row(["Gas [kWh]"] + [round(val, 2) for val in consumption_total_gas])
+    # Electricity [EUR]
+    table.add_row(["Electricity [EUR]"] + [round(val, 2) for val in costs_total_el])
+    # Gas [EUR]
+    table.add_row(["Gas [EUR]"] + [round(val, 2) for val in costs_total_gas])
+    # Electricity [EUR/kWh]
+    table.add_row(["Electricity [EUR/kWh]"] + [round(costs_total_el[i] / consumption_total_el[i], 2) if consumption_total_el[i] else 0 for i in range(len(year_labels))])
+    # Gas [EUR/kWh]
+    table.add_row(["Gas [EUR/kWh]"] + [round(costs_total_gas[i] / consumption_total_gas[i], 2) if consumption_total_gas[i] else 0 for i in range(len(year_labels))])
+    # From
+    from_dates = [df.index[0].date()] + [df.index[year_ends[i-1]+1].date() for i in range(1, len(year_labels))]
+    table.add_row(["From"] + from_dates)
+    # To
+    to_dates = [df.index[year_ends[i]].date() for i in range(len(year_labels))]
+    table.add_row(["To"] + to_dates)
+    print(table)
 
 # Load data
 df = pd.read_csv('EnergyCounter.csv')
 df = preprocess_data(df)
 
 # Consumption per User
+last_day = round(df.Day.iloc[-1])
+year_ends = [365 + 2, 365 * 2 + 3, 365 * 3 + 3, last_day]  # End of Year 1, Year 2, Year 3
+print("Year ends at indices: ", year_ends)
+plot_rolling_year(df, year_ends, mode='ma')
+plot_rolling_year(df, year_ends, mode='cumulative')
+
 ##################### 2022-2023 #####################
-Year1End = 365 + 2
 AriEnd = 287
 
 consumption_ari_y1 = ["Ari", 0, AriEnd, 0.5]
-consumption_quentin_y1 = ["Quentin", AriEnd + 1, Year1End, 0.5]
-consumption_ali_y1 = ["Alexis", 0, Year1End, 0.5]
-consumption_tot_y1 = ["Total", 0, Year1End, 1.0]
+consumption_quentin_y1 = ["Quentin", AriEnd + 1, year_ends[0], 0.5]
+consumption_ali_y1 = ["Alexis", 0, year_ends[0], 0.5]
+consumption_tot_y1 = ["Total", 0, year_ends[0], 1.0]
 
 consumption_y1 = consumption_dict_to_list([consumption_ari_y1, consumption_quentin_y1, consumption_ali_y1, consumption_tot_y1])
 
 consumption_table("Year 1", consumption_y1)
 
 ##################### 2023-2024 #####################
-Year2End = 365 *2 + 3
 QuentinEnd = 365 * 2 - 29
 AnderStart = 365 * 2 - 14
 
-consumption_quentin_y2 = ["Quentin", Year1End+1, QuentinEnd, 0.5]
-consumption_ander_y2 = ["Ander", AnderStart, Year2End, 0.5]
-consumption_ali_y2 = ["Alexis", Year1End+1, Year2End, 0.5]
-consumption_tot_y2 = ["Total", Year1End+1, Year2End, 1.0]
+consumption_quentin_y2 = ["Quentin", year_ends[0]+1, QuentinEnd, 0.5]
+consumption_ander_y2 = ["Ander", AnderStart, year_ends[1], 0.5]
+consumption_ali_y2 = ["Alexis", year_ends[0]+1, year_ends[1], 0.5]
+consumption_tot_y2 = ["Total", year_ends[0]+1, year_ends[1], 1.0]
 
 consumption_y2 = consumption_dict_to_list([consumption_quentin_y2, consumption_ander_y2, consumption_ali_y2, consumption_tot_y2])
 
 consumption_table("Year 2", consumption_y2)
 
 ##################### 2024-2025 #####################
-Year3End = 365 * 3 - 30
-
-consumption_ander_y3 = ["Ander", Year2End+1, Year3End, 0.5]
-consumption_ali_y3 = ["Alexis", Year2End+1, Year3End, 0.5]
-consumption_tot_y3 = ["Total", Year2End+1, Year3End, 1.0]
+consumption_ander_y3 = ["Ander", year_ends[1]+1, year_ends[2], 0.5]
+consumption_ali_y3 = ["Alexis", year_ends[1]+1, year_ends[2], 0.5]
+consumption_tot_y3 = ["Total", year_ends[1]+1, year_ends[2], 1.0]
 
 consumption_y3 = consumption_dict_to_list([consumption_ander_y3, consumption_ali_y3, consumption_tot_y3])
 
 consumption_table("Year 3", consumption_y3)
+
+##################### 2025-2026 #####################
+
+consumption_ander_y4 = ["Ander", year_ends[2]+1, last_day, 0.5]
+consumption_ali_y4 = ["Alexis", year_ends[2]+1, last_day, 0.5]
+consumption_tot_y4 = ["Total", year_ends[2]+1, last_day, 1.0]
+
+consumption_y4 = consumption_dict_to_list([consumption_ander_y4, consumption_ali_y4, consumption_tot_y4])
+
+consumption_table("Year 4", consumption_y4)
 
 ##################### Total Comparison #####################
 
@@ -134,34 +213,17 @@ costsTot_Gas_Y1 = 621.14  / (621.14 + 780.99) * 1356.80
 costsTot_El_Y1 = 780.99 / (621.14 + 780.99) * 1356.80
 costsTot_Gas_Y2 = 210.60 / (210.60 + 421.98) * 662.20
 costsTot_El_Y2 = 421.98 / (210.60 + 421.98) * 662.20
-costsTot_Gas_Y3 = 300 / (300 + 500) * 900
+costsTot_Gas_Y3 = 500 / (300 + 500) * 900
 costsTot_El_Y3 = 500 / (300 + 500) * 900
+# speculation:
 
-print("Total Consumption Comparison over the Years")
+costs_total_el = [costsTot_El_Y1, costsTot_El_Y2, costsTot_El_Y3]
+costs_total_gas = [costsTot_Gas_Y1, costsTot_Gas_Y2, costsTot_Gas_Y3]
+consumption_total_el = [consumption_y1["EL"][-1], consumption_y2["EL"][-1], consumption_y3["EL"][-1]]
+consumption_total_gas = [consumption_y1["GAS"][-1], consumption_y2["GAS"][-1], consumption_y3["GAS"][-1]]
 
-table_Y3 = PrettyTable(["What", "Year 1", "Year 2", "Year 3"])
-# Add rows
-table_Y3.add_row(["Electricity [kWh]", round(consumption_y1["EL"][-1], 2),
-                  round(consumption_y2["EL"][-1], 2),
-                  round(consumption_y3["EL"][-1], 2)])
-table_Y3.add_row(["Gas [kWh]", round(consumption_y1["GAS"][-1], 2),
-                  round(consumption_y2["GAS"][-1], 2),
-                  round(consumption_y3["GAS"][-1], 2)])
-table_Y3.add_row(["Electricity [EUR]", round(costsTot_El_Y1, 2),
-                  round(costsTot_El_Y2, 2),
-                  round(costsTot_El_Y3, 2)])
-table_Y3.add_row(["Gas [EUR]", round(costsTot_Gas_Y1, 2),
-                  round(costsTot_Gas_Y2, 2),
-                  round(costsTot_Gas_Y3, 2)])
-table_Y3.add_row(["Electricity [EUR/kWh]", round(costsTot_El_Y1 / consumption_y1["EL"][-1], 2),
-                  round(costsTot_El_Y2 / consumption_y2["EL"][-1], 2),
-                  round(costsTot_El_Y3 / consumption_y3["EL"][-1], 2)])
-table_Y3.add_row(["Gas [EUR/kWh]", round(costsTot_Gas_Y1 / consumption_y1["GAS"][-1], 2),
-                  round(costsTot_Gas_Y2 / consumption_y2["GAS"][-1], 2),
-                  round(costsTot_Gas_Y3 / consumption_y3["GAS"][-1], 2)])
-table_Y3.add_row(["From", df.index[0].date(), df.index[Year1End+1].date(), df.index[Year2End+1].date()])
-table_Y3.add_row(["To", df.index[Year1End].date(), df.index[Year2End].date(), df.index[Year3End].date()])
-print(table_Y3)
+year_labels = [f"Year {i+1}" for i in range(len(costs_total_el))]
+total_comparison_table(year_labels, consumption_total_el[:len(costs_total_el)], consumption_total_gas[:len(costs_total_gas)], costs_total_el[:len(costs_total_el)], costs_total_gas[:len(costs_total_gas)], df, year_ends)
 
 print("Cost Electricity: ", consumption_y1["EL"][-1] * 0.49)
 print("Cost Gas: ", consumption_y1["GAS"][-1] * 0.33)
@@ -181,35 +243,6 @@ plt.xticks(rotation=45)
 plt.grid()
 plt.legend()
 
-
-fig = plt.figure()
-plt.title('Moving Average Energy Consumption per Day in kWh (Rolling Year)')
-# plt.plot(index_Y1, dE_EL_kWh_MA_Y1, label='Electricity (Y1)')
-plt.plot(df.Day.iloc[0:Year1End-1], df.dE_EL_kWh_MA.iloc[0:Year1End-1], 'mistyrose', label='Electricity (Y1)')
-plt.plot(df.Day.iloc[Year1End:Year2End-1] - df.Day.iloc[Year1End], df.dE_EL_kWh_MA.iloc[Year1End:Year2End-1], 'coral', label='Electricity (Y2)')
-plt.plot(df.Day.iloc[Year2End:-1] - df.Day.iloc[Year2End], df.dE_EL_kWh_MA.iloc[Year2End:-1], 'r', label='Electricity (Y3)')
-plt.plot(df.Day.iloc[0:Year1End-1], df.dE_Gas_kWh_MA.iloc[0:Year1End-1], 'paleturquoise', label='Gas (Y1)')
-plt.plot(df.Day.iloc[Year1End:Year2End-1] - df.Day.iloc[Year1End], df.dE_Gas_kWh_MA.iloc[Year1End:Year2End-1], 'deepskyblue', label='Gas (Y2)')
-plt.plot(df.Day.iloc[Year2End:-1] - df.Day.iloc[Year2End], df.dE_Gas_kWh_MA.iloc[Year2End:-1], 'b', label='Gas (Y3)')
-plt.ylabel('Energy [kWh/d]')
-plt.xlabel('Date')
-plt.xticks(rotation=45)
-plt.grid()
-plt.legend()
-
-fig = plt.figure()
-plt.title('Energy Consumption in kWh (Rolling Year)')
-# plt.plot(index_Y1, dE_EL_kWh_MA_Y1, label='Electricity (Y1)')
-plt.plot(df.Day.iloc[0:Year1End-1], df.Counter_El_kWh.iloc[0:Year1End-1] - df.Counter_El_kWh.iloc[0], 'mistyrose', label='Electricity (Y1)')
-plt.plot(df.Day.iloc[Year1End:Year2End-1] - df.Day.iloc[Year1End], df.Counter_El_kWh.iloc[Year1End:Year2End-1] - df.Counter_El_kWh.iloc[Year1End], 'coral', label='Electricity (Y2)')
-plt.plot(df.Day.iloc[Year2End:-1] - df.Day.iloc[Year2End], df.Counter_El_kWh.iloc[Year2End:-1] - df.Counter_El_kWh.iloc[Year2End], 'r', label='Electricity (Y3)')
-plt.plot(df.Day.iloc[0:Year1End-1], (df.Counter_Gas_kWh.iloc[0:Year1End-1] - df.Counter_Gas_kWh.iloc[0]), 'paleturquoise', label='Gas (Y1)')
-plt.plot(df.Day.iloc[Year1End:Year2End-1] - df.Day.iloc[Year1End], (df.Counter_Gas_kWh.iloc[Year1End:Year2End-1] - df.Counter_Gas_kWh.iloc[Year1End]), 'deepskyblue', label='Gas (Y2)')
-plt.plot(df.Day.iloc[Year2End:-1] - df.Day.iloc[Year2End], (df.Counter_Gas_kWh.iloc[Year2End:-1] - df.Counter_Gas_kWh.iloc[Year2End]), 'b', label='Gas (Y3)')
-plt.ylabel('Energy [kWh]')
-plt.xlabel('Date')
-plt.xticks(rotation=45)
-plt.grid()
-plt.legend()
-
 plt.show()
+
+
